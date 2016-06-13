de8331126fb0d4469205e45d1327b95ab73ef647
John Marlett <1fiyero@gmail.com>
2016-06-13 17:03:03 -0500

    added hero functionality to home.page

--- home.php
+++ home.php
@@ -10,17 +10,20 @@
 get_header();
 ?>
 
-<section>
+<section class="hero"><!-- start of .hero section -->
     
     <ul>
 <?php $args = array('posts_per_page' => 3, 'tag' => 'art, architecture', ) ?>
 
-<?php $home_display = get_posts( $args); 
-        foreach ( $home_display as $post) : setup_postdata($post);  ?>
+<?php $home_display = get_posts( $args); ?>
+       <?php foreach ( $home_display as $post) : setup_postdata($post);  ?>
         
         <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
-        <li>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?> 
+                <div><?php the_post_thumbnail(); ?> </div> 
+            </a> 
+        </li>
         
 <?php endforeach;
     wp_reset_postdata(); ?>
