84458dc3c37aba3fed0759f370d3cfd7ef95b949
John Marlett <1fiyero@gmail.com>
2016-06-20 21:13:08 -0500

    added loop to home page template

--- home.php
+++ home.php
@@ -9,10 +9,21 @@
 get_header(); ?>
 
 <section class="welcome">
+    
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <?php $args = array('tag' => 'welcome', ) ?>
 
 <?php $welcome_msg = get_posts($args); ?>
+
+<?php the_title('<h1>', '</h1>'); ?>
+
+<div> <?php the_content(); ?> </div>
+
+<?php endwhile; ?>
+<?php endif; ?>
+
+<?php wp_reset_postdata(); ?>
     
 </section> <!-- end of .welcome section -->
 
